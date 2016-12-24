9e0a1ed8bffdaea9ee5c5f65726c9180826259f7
destino92 <destinodello@gmail.com>
2016-12-24 18:12:39 +0300

    feat: Integrate React Router to handle page changes

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { link } from 'react-router';
+import { Link } from 'react-router';
 
 const Main = React.createClass({
   render(){
     return (
       <div>
-        <h1>Reduxstagram</h1>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
         {React.cloneElement(this.props.children, this.props)}
       </div>
     )
